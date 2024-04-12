36a3ba3cdf258af70e80f2710e7d6644d520d6a8
tunght994 <tunght994@gmail.com>
2024-04-12 16:54:05 +0700

    update

--- back-end/src/controllers/inventory.controller.js
+++ back-end/src/controllers/inventory.controller.js
@@ -4,10 +4,21 @@ const Inventory = require("../models/Inventory");
 
 const getAllInventory = async (req, res, next) => {
   try {
-    const inventory = await Inventory.find({});
+    let limit = parseInt(req.query.limit) || 0; 
+    let lastId = req.query.lastId;
+    let query = {};
+
+    if (lastId) {
+      query._id = { $gt: lastId }; 
+    }
+
+    const inventory = await Inventory.find(query).limit(limit); 
+    const totalInventoryCount = await Inventory.countDocuments();
+    
     res.status(200).json({
       code: 200,
       data: inventory,
+      totalInventoryCount: totalInventoryCount
     });
   } catch (error) {
     res.status(500).json({ msg: error });
